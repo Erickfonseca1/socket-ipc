e4994fe4bf51e655a7d6b4600de91799f8fba4e3
Erick Fonseca <erick@inovenow.com>
2024-03-17 14:42:02 -0300

    First commit

--- src/com/gugawag/so/ipc/DateClient.java
+++ src/com/gugawag/so/ipc/DateClient.java
@@ -7,7 +7,7 @@ package com.gugawag.so.ipc;
  *
  * @author Silberschatz, Gagne and Galvin. Pequenas alterações feitas por Gustavo Wagner (gugawag@gmail.com)
  * Operating System Concepts  - Eighth Edition
- */ 
+ */
 
 import java.net.*;
 import java.io.*;
@@ -16,7 +16,7 @@ public class DateClient {
 	public static void main(String[] args)  {
 		try {
 			// this could be changed to an IP name or address other than the localhost
-			Socket sock = new Socket("localhost",6013);
+			Socket sock = new Socket("192.168.15.14",6013);
 			InputStream in = sock.getInputStream();
 			BufferedReader bin = new BufferedReader(new InputStreamReader(in));
 
@@ -24,11 +24,11 @@ public class DateClient {
 
 			String line = bin.readLine();
 			System.out.println("O servidor me disse:" + line);
-				
+
 			sock.close();
 		}
 		catch (IOException ioe) {
-				System.err.println(ioe);
+			System.err.println(ioe);
 		}
 	}
 }
